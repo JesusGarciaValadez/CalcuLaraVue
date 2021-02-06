bc2fa2bef50eb76cd6eaa737cf6b9011ed39617d
Jesús Antonio García Valadez <jesus.garciav@me.com>
2021-02-06 00:07:57 -0600

    feat: set up the application

---
+++ resources/views/index.blade.php
@@ -0,0 +1,15 @@
+<x-layout>
+    <div class="bg-white py-16 px-4 sm:px-6 lg:px-8 lg:py-24">
+        <div class="relative max-w-xl mx-auto">
+            <x-calculator.ornament class="absolute left-full transform translate-x-1/2" />
+            <x-calculator.ornament class="absolute right-full bottom-0 transform -translate-x-1/2" />
+
+            <x-calculator.title-section>
+                <x-slot name="title">Calculator</x-slot>
+                Please, select the operation that you want to make.
+            </x-calculator.title-section>
+
+            <x-calculator
+        </div>
+    </div>
+</x-layout>
