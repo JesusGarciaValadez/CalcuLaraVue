a2320ef565c88268f031cb00e7226af1a615d645
Jesús Antonio García Valadez <jesus.garciav@me.com>
2021-02-07 13:00:35 -0600

    tests: add edge cases

--- tests/Feature/ApiTest.php
+++ tests/Feature/ApiTest.php
@@ -2,8 +2,10 @@
 
 namespace Tests\Feature;
 
+use App\Models\Operations;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Arr;
 use Tests\TestCase;
 
 class ApiTest extends TestCase
@@ -19,9 +21,8 @@ class ApiTest extends TestCase
     {
         $operation = '2*2';
         $result = '4';
-        $response = $this->postJson('/api/operations', [ 'operation' => $operation ]);
 
-        $response
+        $this->postJson('/api/operations', [ 'operation' => $operation ])
             ->assertStatus(201)
             ->assertJson([
                 'operation' => $operation,
@@ -29,9 +30,7 @@ class ApiTest extends TestCase
                 'updated_date' => today()->toJSON(),
             ]);
 
-        $response = $this->getJson('/api/operations');
-
-        $response
+        $response = $this->getJson('/api/operations')
             ->assertStatus(200)
             ->assertJsonFragment([
                 'id' => 1,
@@ -50,9 +49,8 @@ class ApiTest extends TestCase
     {
         $operation = '1+2';
         $result = '3';
-        $response = $this->postJson('/api/operations', [ 'operation' => $operation ]);
 
-        $response
+        $this->postJson('/api/operations', [ 'operation' => $operation ])
             ->assertStatus(201)
             ->assertJson([
                 'operation' => $operation,
@@ -62,20 +60,73 @@ class ApiTest extends TestCase
     }
 
     /**
-     * Test making an API request to store an operation with wrong operation
+     * Test making an API request to store an operation with no operation sign
      *
      * @return void
      */
-    public function test_making_an_api_request_to_store_an_operation_with_wrong_operation()
+    public function test_making_an_api_request_to_store_an_operation_with_no_operation_sign()
     {
         $operation = '12';
-        $response = $this->postJson('/api/operations', [ 'operation' => $operation ]);
 
-        $response
+        $this->postJson('/api/operations', [ 'operation' => $operation ])
             ->assertStatus(400)
             ->assertJson([
                 'error' => 'Operation not allowed',
-                'exception' => 'There\'s not an operation to do.',
+                'exception' => 'There\'s not a valid operation to do.',
+                'operation' => $operation,
+            ]);
+    }
+
+    /**
+     * Test making an API request to store an operation with letters
+     *
+     * @return void
+     */
+    public function test_making_an_api_request_to_store_an_operation_with_letters()
+    {
+        $operation = 'abc';
+
+        $this->postJson('/api/operations', [ 'operation' => $operation ])
+            ->assertStatus(400)
+            ->assertJson([
+                'error' => 'Operation not allowed',
+                'exception' => 'There\'s not a valid operation to do.',
+                'operation' => $operation,
+            ]);
+    }
+
+    /**
+     * Test making an API request to store an operation with a number and a letter
+     *
+     * @return void
+     */
+    public function test_making_an_api_request_to_store_an_operation_with_a_number_and_a_letter()
+    {
+        $operation = 'a+2';
+
+        $this->postJson('/api/operations', [ 'operation' => $operation ])
+            ->assertStatus(400)
+            ->assertJson([
+                'error' => 'Calculation Error',
+                'exception' => 'One of the operands is not a valid number.',
+                'operation' => $operation,
+            ]);
+    }
+
+    /**
+     * Test making an API request to store an operation with a number and a letter
+     *
+     * @return void
+     */
+    public function test_making_an_api_request_to_store_an_operation_with_only_number_and_a_letter()
+    {
+        $operation = '2+';
+
+        $this->postJson('/api/operations', [ 'operation' => $operation ])
+            ->assertStatus(400)
+            ->assertJson([
+                'error' => 'Calculation Error',
+                'exception' => 'One of the operands is not present.',
                 'operation' => $operation,
             ]);
     }
@@ -87,28 +138,15 @@ class ApiTest extends TestCase
      */
     public function test_try_to_retrieve_a_list_of_stored_operations_after_delete_all_the_operations()
     {
-        $operation = '2*2';
-        $result = '4';
-        $response = $this->postJson('/api/operations', [ 'operation' => $operation ]);
-
-        $response
-            ->assertStatus(201)
-            ->assertJson([
-                'operation' => $operation,
-                'result' => $result,
-                'updated_date' => today()->toJSON(),
-            ]);
+        $operations = Operations::factory()->count(10)->create();
 
         $this->deleteJson('/api/operations');
 
-        $response = $this->getJson('/api/operations');
-
-        $response
+        $this->getJson('/api/operations')
             ->assertStatus(200)
             ->assertJsonMissing([
-                'id' => 1,
-                'operation' => $operation,
-                'result' => $result,
+                'operation' => $operations->get(0)->operation,
+                'result' => $operations->get(0)->result,
                 'updated_date' => today()->toJSON(),
             ]);
     }
@@ -120,39 +158,17 @@ class ApiTest extends TestCase
      */
     public function test_try_to_retrieve_a_list_of_stored_operations_after_delete_one_operation()
     {
-        $operationOne = '2*2';
-        $resultOne = '4';
-        $responseOne = $this->postJson('/api/operations', [ 'operation' => $operationOne ]);
-        $responseOne
-            ->assertStatus(201)
-            ->assertJson([
-                'operation' => $operationOne,
-                'result' => $resultOne,
-                'updated_date' => today()->toJSON(),
-            ]);
+        $operations = Operations::factory()->count(10)->create();
 
-        $operationTwo = '8*8';
-        $resultTwo = '64';
-        $responseTwo = $this->postJson('/api/operations', [ 'operation' => $operationTwo ]);
-        $responseTwo
-            ->assertStatus(201)
-            ->assertJson([
-                'operation' => $operationTwo,
-                'result' => $resultTwo,
-                'updated_date' => today()->toJSON(),
-            ]);
+        $this->deleteJson('/api/operations/' . $operations[0]->id);
 
-        $responseThree = $this->getJson('/api/operations');
-
-        $responseFour = $this->deleteJson('/api/operations/' . $responseThree[0]['id']);
-
-        $responseFive = $this->getJson('/api/operations');
-        $responseFive->assertStatus(200)
-            ->assertJsonFragment([
-                'id' => 2,
-                'operation' => $operationTwo,
-                'result' => $resultTwo,
-                'updated_date' => today()->toJSON(),
+        $this->getJson('/api/operations')
+            ->assertStatus(200)
+            ->assertJsonMissingExact([
+                'id' => $operations[0]->id,
+                'operation' => $operations[0]->operation,
+                'result' => $operations[0]->result,
+                'updated_date' => $operations[0]->updated_date,
             ]);
     }
 }
